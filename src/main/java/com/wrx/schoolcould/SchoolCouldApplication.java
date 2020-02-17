161dbbb7421a486d51782f7366758bea4d98da0b
1145486986@qq.com <wrx08.4013.com>
2020-02-18 07:47:57 +0800

    first

---
+++ src/main/java/com/wrx/schoolcould/SchoolCouldApplication.java
@@ -0,0 +1,13 @@
+package com.wrx.schoolcould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCouldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolCouldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wrx/schoolcould/SchoolCouldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrx.schoolcould;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCouldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
